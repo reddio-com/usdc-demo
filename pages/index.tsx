66fc090b28da5e8378c0a11b20f59b3584d883a7
KieSun <zx597813039@gmail.com>
2023-08-12 21:15:12 +0800

    feat: init project

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,131 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import React from 'react'
+import * as Tabs from '@radix-ui/react-tabs'
 
-const inter = Inter({ subsets: ['latin'] })
-
-export default function Home() {
-  return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
+const TabsDemo = () => (
+  <main className='flex justify-center items-center h-screen'>
+    <Tabs.Root className="TabsRoot" defaultValue="tab1">
+      <Tabs.List className="TabsList" aria-label="Manage your account">
+        <Tabs.Trigger className="TabsTrigger" value="tab1">
+          Deposit
+        </Tabs.Trigger>
+        <Tabs.Trigger className="TabsTrigger" value="tab2">
+          Withdraw
+        </Tabs.Trigger>
+        <Tabs.Trigger className="TabsTrigger" value="tab3">
+          Withdraw Area
+        </Tabs.Trigger>
+        <Tabs.Trigger className="TabsTrigger" value="tab4">
+          Balance
+        </Tabs.Trigger>
+      </Tabs.List>
+      <Tabs.Content className="TabsContent" value="tab1">
+        <p className="Text">
+          Make changes to your account here. Click save when you're done.
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+        <fieldset className="Fieldset">
+          <label className="Label" htmlFor="name">
+            Name
+          </label>
+          <input className="Input" id="name" defaultValue="Pedro Duarte" />
+        </fieldset>
+        <fieldset className="Fieldset">
+          <label className="Label" htmlFor="username">
+            Username
+          </label>
+          <input className="Input" id="username" defaultValue="@peduarte" />
+        </fieldset>
+        <div
+          style={{ display: 'flex', marginTop: 20, justifyContent: 'flex-end' }}
+        >
+          <button className="Button green">Save changes</button>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+      </Tabs.Content>
+      <Tabs.Content className="TabsContent" value="tab2">
+        <p className="Text">
+          Change your password here. After saving, you'll be logged out.
+        </p>
+        <fieldset className="Fieldset">
+          <label className="Label" htmlFor="currentPassword">
+            Current password
+          </label>
+          <input className="Input" id="currentPassword" type="password" />
+        </fieldset>
+        <fieldset className="Fieldset">
+          <label className="Label" htmlFor="newPassword">
+            New password
+          </label>
+          <input className="Input" id="newPassword" type="password" />
+        </fieldset>
+        <fieldset className="Fieldset">
+          <label className="Label" htmlFor="confirmPassword">
+            Confirm password
+          </label>
+          <input className="Input" id="confirmPassword" type="password" />
+        </fieldset>
+        <div
+          style={{ display: 'flex', marginTop: 20, justifyContent: 'flex-end' }}
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+          <button className="Button green">Change password</button>
+        </div>
+      </Tabs.Content>
+      <Tabs.Content className="TabsContent" value="tab3">
+        <p className="Text">
+          Change your password here. After saving, you'll be logged out.
+        </p>
+        <fieldset className="Fieldset">
+          <label className="Label" htmlFor="currentPassword">
+            Current password
+          </label>
+          <input className="Input" id="currentPassword" type="password" />
+        </fieldset>
+        <fieldset className="Fieldset">
+          <label className="Label" htmlFor="newPassword">
+            New password
+          </label>
+          <input className="Input" id="newPassword" type="password" />
+        </fieldset>
+        <fieldset className="Fieldset">
+          <label className="Label" htmlFor="confirmPassword">
+            Confirm password
+          </label>
+          <input className="Input" id="confirmPassword" type="password" />
+        </fieldset>
+        <div
+          style={{ display: 'flex', marginTop: 20, justifyContent: 'flex-end' }}
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+          <button className="Button green">Change password</button>
+        </div>
+      </Tabs.Content>
+      <Tabs.Content className="TabsContent" value="tab4">
+        <p className="Text">
+          Change your password here. After saving, you'll be logged out.
+        </p>
+        <fieldset className="Fieldset">
+          <label className="Label" htmlFor="currentPassword">
+            Current password
+          </label>
+          <input className="Input" id="currentPassword" type="password" />
+        </fieldset>
+        <fieldset className="Fieldset">
+          <label className="Label" htmlFor="newPassword">
+            New password
+          </label>
+          <input className="Input" id="newPassword" type="password" />
+        </fieldset>
+        <fieldset className="Fieldset">
+          <label className="Label" htmlFor="confirmPassword">
+            Confirm password
+          </label>
+          <input className="Input" id="confirmPassword" type="password" />
+        </fieldset>
+        <div
+          style={{ display: 'flex', marginTop: 20, justifyContent: 'flex-end' }}
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
+          <button className="Button green">Change password</button>
+        </div>
+      </Tabs.Content>
+    </Tabs.Root>
+  </main>
+)
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default TabsDemo
